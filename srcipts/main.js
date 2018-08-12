cf455729c6436f3140a2e0f975e4456f8480ec7d
bobo1967 <1215373008@qq.com>
2018-08-12 23:01:43 +0800

    1

--- srcipts/main.js
+++ srcipts/main.js
@@ -2,17 +2,18 @@
  * 异步加载地图
  */
 (function() {
-    var url = 'https://webapi.amap.com/maps?v=1.4.8&key=38db8101e26b0719fd8148bd78bde6f9&callback=onLoad',
+    var url = 'https://webapi.amap.com/maps?v=1.4.8&key=38db8101e26b0719fd8148bd78bde6f9&callback=loadMap',
+        UIurl = 'https://webapi.amap.com/ui/1.0/main.js?v=1.0.11',
         jsapi = document.createElement('script');
-       
-    jsapi.charset = 'utf-8';
+        ui = document.createElement('script');
+        
+    //加载地图js文件
     jsapi.src = url;
     document.head.appendChild(jsapi);
+    //加载UI文件
+    ui.src = UIurl;
+    document.head.appendChild(ui);
 })();
-
-window.onLoad  = function(){
-    loadMap();
-};
 
 /**
  * 加载地图和插件
@@ -30,7 +31,81 @@ function loadMap() {
        });
        //异步加载插件
        AMap.plugin(['AMap.ToolBar', 'AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Geolocation'], pluginOptions);
+       //启动UI
+       activeUI();
+    
+      
    }, 1500);
+}
+/**
+ * 轨迹巡航配置
+ */
+function activeUI() {
+    
+    AMapUI.load(['ui/misc/PathSimplifier'], function(PathSimplifier) {
+        if (!PathSimplifier.supportCanvas) {
+            alert('当前环境不支持 Canvas！');
+            return;
+        }
+        //启动页面
+        initPage(PathSimplifier);
+
+    });
+
+    function initPage(PathSimplifier) {
+        //创建组件实例
+        var pathSimplifierIns = new PathSimplifier({
+            zIndex: 100,
+            map: map, //所属的地图实例
+            getPath: function (pathData, pathIndex) {
+                //返回轨迹数据中的节点坐标信息，[AMap.LngLat, AMap.LngLat...] 或者 [[lng|number,lat|number],...]
+                return pathData.path;
+            },
+            getHoverTitle: function (pathData, pathIndex, pointIndex) {
+                //返回鼠标悬停时显示的信息
+                if (pointIndex >= 0) {
+                    //鼠标悬停在某个轨迹节点上
+                    return pathData.name + '，点:' + pointIndex + '/' + pathData.path.length;
+                }
+                //鼠标悬停在节点之间的连线上
+                return pathData.name + '，点数量' + pathData.path.length;
+            },
+            renderOptions: {
+                //轨迹线的样式
+                pathLineStyle: {
+                    strokeStyle: 'red',
+                    lineWidth: 6,
+                    dirArrowStyle: true
+                }
+            }
+        });
+
+        //在这里设置数据
+        pathSimplifierIns.setData([
+            {
+                name: '轨迹0',
+                path: [
+                    // [100.340417, 27.376994],
+        
+                    // [108.426354, 37.827452],
+                    // [113.392174, 31.208439],
+                    // [124.905846, 42.232876]
+                    // [113.32703, 23.132175],
+                    // [113.393116, 23.039404]
+                ]
+            }, 
+        ]);
+
+        //创建一个巡航器
+        var navg0 = pathSimplifierIns.createPathNavigator(0, //关联第1条轨迹
+            {
+                loop: false, //循环播放
+                speed: 1000000
+            });
+
+        navg0.start();
+        // pathSimplifierIns.hide();
+    }
 }
 
 /**
@@ -108,6 +183,7 @@ function pluginOptions() {
     function select(e) {
         placeSearch.setCity(e.poi.adcode);
         placeSearch.search(e.poi.name);  //关键字查询查询
+        console.log(e.poi);
     }
     //获取选取点的信息
     function getMakerData(e) {
@@ -253,3 +329,4 @@ function pluginOptions() {
     EventUtil.addHandler($('.part-right')[0], 'click', partRightClickListen);
     
 })();
+
