6927a50f7b939ecbc3c17c1d541f0cde8685c7f9
bobo1967 <1215373008@qq.com>
2018-08-12 11:23:13 +0800

    1

--- srcipts/main.js
+++ srcipts/main.js
@@ -1,42 +1,119 @@
 /**
  * 异步加载地图
  */
-window.onLoad  = function(){
-    
-    setTimeout(() => {
-        //移除加载动画
-        partRight.removeChild(loading);
-        var map = new AMap.Map('map-container', {
-            zoom: 10,
-            //广州市区的坐标
-            center: [113.23, 23.13],
-            
-        });
-        AMap.plugin(['AMap.ToolBar','AMap.Autocomplete'],function() {//异步加载插件
-            var toolbar = new AMap.ToolBar({
-                "direction": false,
-                "position": "RB" //将插件置于右下方
-            });
-            map.addControl(toolbar);
-            // 实例化Autocomplete
-            var autoOptions = {
-                //city 限定城市，默认全国
-                city: '广州',
-                input: ''
-            };
-            var autoComplete = new AMap.Autocomplete(autoOptions);
-           
-        });
-    }, 2500);
-};
-var url = 'https://webapi.amap.com/maps?v=1.4.8&key=38db8101e26b0719fd8148bd78bde6f9&callback=onLoad',
-    jsapi = document.createElement('script'),
-    partRight = document.getElementsByClassName('panel-right-container')[0],
-    loading = document.getElementsByClassName('loading-container')[0];
+(function() {
+    var url = 'https://webapi.amap.com/maps?v=1.4.8&key=38db8101e26b0719fd8148bd78bde6f9&callback=onLoad',
+        jsapi = document.createElement('script');
+       
+    jsapi.charset = 'utf-8';
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+})();
 
-jsapi.charset = 'utf-8';
-jsapi.src = url;
-document.head.appendChild(jsapi);
+window.onLoad  = function(){
+    loadMap();
+};
+
+/**
+ * 加载地图和插件
+ */
+function loadMap() {
+   setTimeout(() => {
+       var partRight = document.getElementsByClassName('panel-right-container')[0],
+           loading = document.getElementsByClassName('loading-container')[0];
+       //移除加载动画
+       partRight.removeChild(loading);
+       window.map = new AMap.Map('map-container', {
+           zoom: 10,
+           //广州市区的坐标
+           center: [113.23, 23.13],
+       });
+       //异步加载插件
+       AMap.plugin(['AMap.ToolBar', 'AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Geolocation'], pluginOptions);
+   }, 1500);
+}
+
+/**
+ * 插件的选项
+ */
+function pluginOptions() {
+
+    //缩放工具条
+    var toolbar = new AMap.ToolBar({
+        "direction": false,
+        "position": "RB", //将插件置于右下方
+        "locate": false
+    });
+    map.addControl(toolbar);
+    //定位插件
+    window.geolocation = new AMap.Geolocation({
+        enableHighAccuracy: true, //使用高精度定位
+        timeout: 10000, //超过10秒后停止定位，默认：无穷大
+        showButton: false,
+        zoomToAccuracy: true, //定位成功后调整地图视野范围使定位位置及精度范围视野内可见
+    });
+    map.addControl(geolocation);
+    AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
+    AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
+    //解析定位结果
+    function onComplete(data) {
+        var str = ['定位成功'];
+        str.push('经度：' + data.position.getLng());
+        str.push('纬度：' + data.position.getLat());
+        if (data.accuracy) {
+            str.push('精度：' + data.accuracy + ' 米');
+        } //如为IP精确定位结果则没有精度信息
+        str.push('是否经过偏移：' + (data.isConverted ? '是' : '否'));
+        console.log(str);
+    }
+    //解析定位错误信息
+    function onError(data) {
+        console.log('locate error');
+    }
+    // 实例化Autocomplete
+    var autoSearch = new AMap.Autocomplete({
+        //city 限定城市，默认全国
+        city: '广州',
+        citylimit: true,
+        input: 'search-input'
+    });
+    var autoStart = new AMap.Autocomplete({
+        //city 限定城市，默认全国
+        city: '广州',
+        citylimit: true,
+        input: 'start-input'
+    });
+    var autoEnd = new AMap.Autocomplete({
+        //city 限定城市，默认全国
+        city: '广州',
+        citylimit: true,
+        input: 'end-input'
+    });
+    //地图搜索选项
+    const placeoptions = {
+        map: map,
+        autoFitView: true
+    };
+    //构造地点查询类
+    var placeSearch = new AMap.PlaceSearch(placeoptions);  
+    // var startSearch = new AMap.PlaceSearch(placeoptions);  
+    // var endSearch = new AMap.PlaceSearch(placeoptions);  
+    //注册监听，当选中某条记录时会触发
+    AMap.event.addListener(autoSearch, 'select', select);
+    AMap.event.addListener(autoStart, 'select', select);
+    AMap.event.addListener(autoEnd, 'select', select);
+    AMap.event.addListener(placeSearch, 'markerClick', getMakerData);
+    // AMap.event.addListener(startSearch, 'markerClick', getMakerData);
+    // AMap.event.addListener(endSearch, 'markerClick', getMakerData);
+    function select(e) {
+        placeSearch.setCity(e.poi.adcode);
+        placeSearch.search(e.poi.name);  //关键字查询查询
+    }
+    //获取选取点的信息
+    function getMakerData(e) {
+        console.log(e.data);
+    }     
+}
 
 /**
  * 引入多个插件
@@ -96,34 +173,39 @@ document.head.appendChild(jsapi);
     // layer.render();
 
 
+(function () {
+    /**
+     * 隐藏或者显示左面板功能
+     */
+    var panel = document.getElementsByClassName('panel-left-container')[0];
+    showPanelButton = document.getElementsByClassName('panel-button')[0];
+
+    EventUtil.addHandler(showPanelButton, 'click', function() {
+        // ClassUtil.toggleClass(panel, 'hide-panel');
+        if (ClassUtil.hasClass(panel, 'hide-panel')) {
+            ClassUtil.removeClass(panel, 'hide-panel');
+            showPanelButton.innerHTML = '‹';
+        } else {
+            ClassUtil.addClass(panel, 'hide-panel');
+            showPanelButton.innerHTML = '›';
+        }
+    });
+
+    /**
+     * 显示二级菜单
+     */
+    var navFirst = document.getElementsByClassName('nav-1'),
+        showNavButton = document.getElementsByClassName('show-nav-button ');
+
     (function () {
-        /**
-         * 隐藏或者显示左面板功能
-         */
-        var panel = document.getElementsByClassName('panel-left-container')[0];
-        showPanelButton = document.getElementsByClassName('panel-button')[0];
-    
-        EventUtil.addHandler(showPanelButton, 'click', function() {
-            ClassUtil.toggleClass(panel, 'hide-panel');
-        });
-    
-        /**
-         * 显示二级菜单
-         */
-        var navFirst = document.getElementsByClassName('nav-1'),
-            showNavButton = document.getElementsByClassName('show-nav-button ');
-    
-        (function () {
-            for (let i = 0; i < showNavButton.length; i++) {
-                EventUtil.addHandler(showNavButton[i], 'click', function() {
-                    ClassUtil.toggleClass(navFirst[i], 'show-nav-animatiton');
-                });
-            }
-        })();
+        for (let i = 0; i < showNavButton.length; i++) {
+            EventUtil.addHandler(showNavButton[i], 'click', function() {
+                ClassUtil.toggleClass(navFirst[i], 'show-nav-animatiton');
+            });
+        }
     })();
+})();
     
-
-
 (function() {
     /**
      * @version 1.0
@@ -159,6 +241,10 @@ document.head.appendChild(jsapi);
                     return;
                 }
                 dateAreaAnimate();
+                break;
+            }
+            case $('.part-right .switch-box img')[1]: {
+                geolocation.getCurrentPosition(); //定位调用语句
                 break;
             }
         }
