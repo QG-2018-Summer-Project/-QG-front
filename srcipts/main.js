f467a6ed1a0ed124f37b8f2d4263fc1718e3f2c4
bobo1967 <1215373008@qq.com>
2018-08-14 22:48:13 +0800

    1

--- srcipts/main.js
+++ srcipts/main.js
@@ -30,17 +30,192 @@ function loadMap() {
             zoom: 10,
             //广州市区的坐标
             center: [113.23, 23.13],    
-            rotateEnable: true
+            rotateEnable: true,
+            isHotspot: true,
         });
         //异步加载插件
-        AMap.plugin(['AMap.ToolBar', 'AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Geolocation', 'AMap.ControlBar'], pluginOptions);
+        AMap.plugin(['AMap.ToolBar', 'AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Geolocation', 'AMap.ControlBar', 'AMap.Driving'], pluginOptions);
         //启动UI
         // activeUI();
-        geolocation.getCurrentPosition(); //定位调用语句
+        //初始化定位
+        geolocation.getCurrentPosition(); 
+        //开启地图热点功能
+        activeHotSpot();
    }, 1500);
-   
-  
 }
+
+/**
+ * 地图热点功能
+ */
+function activeHotSpot() {
+    var placeSearch = new AMap.PlaceSearch({
+        city: "广州",
+        citylimit: true,
+        autoFitView: true
+    });
+    var clickEventListener = map.on('hotspotclick', function(e) {
+        console.log('定位成功:' + '[' + e.lnglat.getLng() + ',' + e.lnglat.getLat() + ']');
+        map.setZoomAndCenter(17, [e.lnglat.getLng(), e.lnglat.getLat()]);
+        
+        placeSearch.getDetails(e.id, function(status, result) {
+            console.log(result);
+            if (status === 'complete' && result.info === 'OK') {
+                //开启信息窗口
+                var poiInfo = result.poiList.pois[0];
+                
+                showInfoWindow(poiInfo.name, poiInfo.location);
+                
+            } 
+        });
+    });
+}
+function setStartContent(content) {
+    var startInput = document.getElementById('start-input');
+    startInput.value = content;
+}
+function setEndContent(content) {
+    var endInput = document.getElementById('end-input');
+    endInput.value = content;
+}
+
+/**
+ * 展示详细信息窗口功能
+ * @param {string} content 
+ * @param {object} location 
+ */
+function showInfoWindow(content, location) {
+    
+    var infoWindow = new AMap.InfoWindow({
+        isCustom: true,  //使用自定义窗体
+        content:    '<div id="info-box"><div id="close-container"><button id="close-button">x</button></div>' +
+                    '<p id="content">'+ content + '</p>' +                    
+                    '<div class="set-button-container">' +
+                    '<button id="start-button" class="set-button">设为起点</button>' +
+                    '<button id="end-button" class="set-button">到这里去</button></div></div>',
+        offset: new AMap.Pixel(-2, -22) //left: -2, top: -20
+    });
+    
+    
+    infoWindow.open(map, location);
+        
+    //手动异步
+
+    setTimeout(() => {
+        var closeButton = document.getElementById('close-button'),
+            startButton = document.getElementById('start-button'),
+            endButton = document.getElementById('end-button');
+
+        EventUtil.addHandler(startButton, 'click', function() {
+            selectMarker(0);
+            setStartContent(content);
+            EventUtil.removeHandler(startButton, 'click');
+        });
+        EventUtil.addHandler(endButton, 'click', function() {
+            selectMarker(1);
+            setEndContent(content);
+            EventUtil.removeHandler(endButton, 'click');
+        });
+        EventUtil.addHandler(closeButton, 'click', function() {
+            selectMarker();
+            EventUtil.removeHandler(closeButton, 'click');
+        });
+    }, 0);
+    /**
+     * 点击不同的按钮有不同的图标
+     * @param {int} type 
+     */
+    function selectMarker(type) {
+        switch(type) {
+            case 0: { //设置为起点
+                if (map.hasOwnProperty('startMarker')) {
+                    map.remove(map.startMarker);
+                } 
+                if (map.hasOwnProperty('selectMarker')) {
+                    map.remove(map.selectMarker);
+                } 
+                if (map.hasOwnProperty('endMarker')) {
+                    map.remove(map.endMarker);
+                }
+                //把点加到地图对象上
+                map.startMarker = addMarker(location, -9, -3);
+                infoWindow.close();
+                break;
+            } case 1: { //设置为终点
+                if (map.hasOwnProperty('startMarker')) {
+                    map.remove(map.startMarker);
+                } 
+                if (map.hasOwnProperty('endMarker')) {
+                    map.remove(map.endMarker);
+                }
+                if (map.hasOwnProperty('selectMarker')) {
+                    map.remove(map.selectMarker);
+                }
+                map.endMarker = addMarker(location, -97, -3);
+                //开始导航
+                startRoute(map.startMarker.getPosition(), location);
+                infoWindow.close();
+                break;
+            } default: {
+                infoWindow.close();
+            }
+        }
+    }
+}
+
+function startRoute(start, end) {
+    var driving = new AMap.Driving({
+        map: map,
+        hideMarkers: true,
+        showTraffic: false,
+        extensions: 'all',
+        outlineColor: 'black',
+        Policy: 0
+    }); 
+
+    // driving.setPolicy(1);
+    // driving.setPolicy(2);
+    // driving.setPolicy(4);
+    
+    driving.search(start, end, function(status, result) {
+        //driving.clear();
+        if (status === 'no_data') {
+            alert('没有结果');
+        } else {
+            console.log(result);
+        }
+    });
+
+    
+    
+}
+
+/**
+ * 在地图上添加一个点
+ * @param {*} location 
+ * @param {*} offLeft 
+ * @param {*} offTop 
+ */
+function addMarker(location, offLeft, offTop) {
+    var marker = new AMap.Marker({
+        position: location,
+        animation: 'AMAP_ANIMATION_DROP',
+        map: map,
+
+        icon: new AMap.Icon({            
+            size: new AMap.Size(25, 34),  //图标大小
+            image: "../images/poi-marker.png",
+            imageSize: [437, 267],
+            imageOffset: new AMap.Pixel(offLeft, offTop)
+        }) 
+    });
+    
+
+    AMap.event.addListener(marker, 'click', function() {
+        
+    });
+    return marker;
+}
+
 /**
  * 轨迹巡航配置
  */
@@ -117,30 +292,32 @@ function activeUI() {
  * 插件的选项
  */
 function pluginOptions() {
-
+    //定位插件
     window.geolocation = new AMap.Geolocation({
-        enableHighAccuracy: true, //使用高精度定位
+        //enableHighAccuracy: true, //使用高精度定位
         timeout: 10000, //超过10秒后停止定位，默认：无穷大
         showButton: false,
         zoomToAccuracy: true, //定位成功后调整地图视野范围使定位位置及精度范围视野内可见
     });
+
     map.addControl(geolocation);
     AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
     AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
+
     //解析定位结果
     function onComplete(data) {
-        var str = ['定位成功'];
-        str.push('经度：' + data.position.getLng());
-        str.push('纬度：' + data.position.getLat());
-        if (data.accuracy) {
-            str.push('精度：' + data.accuracy + ' 米');
-        } //如为IP精确定位结果则没有精度信息
-        str.push('是否经过偏移：' + (data.isConverted ? '是' : '否'));
-        console.log(str);
+        // var str = ['定位成功'];
+        // str.push('经度：' + data.position.getLng());
+        // str.push('纬度：' + data.position.getLat());
+        // if (data.accuracy) {
+        //     str.push('精度：' + data.accuracy + ' 米');
+        // } //如为IP精确定位结果则没有精度信息
+        // str.push('是否经过偏移：' + (data.isConverted ? '是' : '否'));
+        console.log('定位成功');
     }
     //解析定位错误信息
     function onError(data) {
-        console.log('locate error');
+       console.log('定位失败');
     }
     // 实例化Autocomplete
     var autoSearch = new AMap.Autocomplete({
@@ -149,8 +326,6 @@ function pluginOptions() {
         citylimit: true,
         input: 'search-input'
     });
-
-    
     var autoStart = new AMap.Autocomplete({
         //city 限定城市，默认全国
         city: '广州',
@@ -163,127 +338,34 @@ function pluginOptions() {
         citylimit: true,
         input: 'end-input'
     });
-    //地图搜索选项
-    // const placeoptions = {
-    //     map: map,
-    //     autoFitView: true,
-    //     pageSize: 1 //只返回一个结果
-    // };
-    // //构造地点查询类
-    // var placeSearch = new AMap.PlaceSearch(placeoptions);  
-    // var startSearch = new AMap.PlaceSearch(placeoptions);  
-    // var endSearch = new AMap.PlaceSearch(placeoptions);  
+    
     //注册监听，当选中某条记录时会触发
     AMap.event.addListener(autoSearch, 'select', select);
     AMap.event.addListener(autoStart, 'select', select);
     AMap.event.addListener(autoEnd, 'select', select);
-    // AMap.event.addListener(placeSearch, 'markerClick', getMakerData);
-    // AMap.event.addListener(startSearch, 'markerClick', getMakerData);
-    // AMap.event.addListener(endSearch, 'markerClick', getMakerData);
     
     function select(e) {
         console.log(e.poi);
         // 设置缩放级别和中心点
         map.setZoomAndCenter(14, [e.poi.location.lng, e.poi.location.lat]);
-        // 在新中心点添加 marker 
-        var marker = new AMap.Marker({
+        //把搜索得出的marker加到地图上 
+        map.selectMarker = new AMap.Marker({
             map: map,
             position: [e.poi.location.lng, e.poi.location.lat],
             animation: 'AMAP_ANIMATION_DROP',
         });
-        AMap.event.addListener(marker, 'click', function() {
-            showInfoWindow(e.poi.name, marker);
-            
+        AMap.event.addListener(map.selectMarker, 'click', function() {
+            showInfoWindow(e.poi.name, map.selectMarker.getPosition());
+            console.log(map.selectMarker.getPosition());
         });
-
     }
 
-    //获取选取点的信息
-    function getMakerData(event) {
-        console.log(event.data);
-        console.log(event.marker);
-        AMap.event.addListener(event.marker, 'click', function() {
-           
-        });
-    }     
 }
-function creatInfoWindow() {
-    
-}
-function showInfoWindow(content, marker) {
-    var infoWindow = new AMap.InfoWindow({
-        isCustom: true,  //使用自定义窗体
-        content:    '<div id="info-box"><div id="close-container"><button id="close-button">x</button></div>' +
-                    '<p id="content">'+ content + '</p>' +                    
-                    '<div class="set-button-container">' +
-                    '<button id="start" class="set-button">设为起点</button>' +
-                    '<button id="end" class="set-button">到这里去</button></div></div>',
-        offset: new AMap.Pixel(-2, -50)
-    });
-    infoWindow.open(map, marker.getPosition());
-}
-/** 
- * 引入多个插件
- */
 
 /**
- * 热力图 
- * 
-*/
-// var loca = Loca.create('container', {
-    //     mapStyle: 'amap://styles/midnight',
-    //     zoom: 11,
-    //     center: [116.397428, 39.90923]
-    // });
-    // //创建图层
-    // var layer = Loca.visualLayer({
-    //     container: loca,
-    //     type: 'heatmap',
-    //     shape: 'normal'
-    // });
-    // //数据
-    // for (var i = 0; i < heatmapData.length; i++) {
-    //     console.log(heatmapData[i]);
-    // }
-
-    // var list = [];
-    // var i = -1, length = heatmapData.length;
-    // while (++i < length) {
-    //     var item = heatmapData[i];
-    //     list.push({
-    //         coordinate: [item.lng, item.lat],
-    //         count: item.count
-    //     })
-    // }
-
-    // layer.setData(list, {
-    //     lnglat: 'coordinate',
-    //     value: 'count'
-    // });
-    // //添加数据
-    
-    // //设置样式
-    // layer.setOptions({
-    //     style: {
-    //         radius: 25,
-    //         opacity: [0.1, 0.9],
-    //     },
-    //     gradient: {
-    //         0.5: '#2c7bb6',
-    //         0.65: '#abd9e9',
-    //         0.7: '#ffffbf',
-    //         0.9: '#fde468',
-    //         1.0: '#d7191c'
-    //     }
-    // });
-    // //绘制图层
-    // layer.render();
-
-
-(function () {
-    /**
-     * 隐藏或者显示左面板功能
-     */
+ * 隐藏或者显示左面板功能
+ */
+(function () {    
     var panel = document.getElementsByClassName('panel-left-container')[0];
     showPanelButton = document.getElementsByClassName('panel-button')[0];
 
@@ -312,13 +394,13 @@ function showInfoWindow(content, marker) {
         }
     })();
 })();
-    
-(function() {
-    /**
+/**
      * @version 1.0
      * @author
      * @description 将选择的时间段区域进行展开或者缩小，当宽度为520时候缩小，当宽度为0时候展开。由于弹出串口没有做到适应窗口大小，所以还未定稿
-     */
+     */    
+(function() {
+    
     function dateAreaAnimate() {
         if ($('.date-switch-container').css('width') == '560px') {
             $('.date-switch-container').animate({
@@ -360,4 +442,57 @@ function showInfoWindow(content, marker) {
     EventUtil.addHandler($('.part-right')[0], 'click', partRightClickListen);
     
 })();
+
+function analysisRouteData(data, index) {
+    console.log(data);
+
+    var route = [ //路径
+        {
+            index: index.toString(), //第几条路线
+            steps: [ //要经历的步骤
+                {
+                    path: [ //此路段坐标集合
+                        {
+
+                        }
+                    ],
+                    start_location: {},
+                    end_location: {},
+                    time: "", //经过这个路段要多久
+                }
+            ],
+            allTime: "", //高德地图预测的时间
+            distance: "" //起点和终点之间的距离
+        }
+    ];
+    route[0].allTime = data.time.toString();
+    route[0].distance = data.distance.toString();
+
+    for (let i = 0; i < data.steps.length; i++) {
+        route[0].steps[i].path = [];
+
+        route[0].steps[i].start_location = {
+            lon: data.steps[i].start_location.lng,
+            lat: data.steps[i].start_location.lat
+        };
+        route[0].steps[i].end_location = {
+            lon: data.steps[i].end_location.lng,
+            lat: data.steps[i].end_location.lat,
+        };
+        route[0].steps[i].time = data.steps[i].time.toString();
+
+        route[0].steps.push({
+
+        });
+        for (let j = 0; j < data.steps[i].path.length; j++) {
+            route[0].steps[i].path[j] = {
+                lon: data.steps[i].path[j].lng,
+                lat: data.steps[i].path[j].lat
+            }
+        }
+    }
+    //去除最后一个数组项
+    route[0].steps.pop();
+    console.log(route[0]);
+}
 
